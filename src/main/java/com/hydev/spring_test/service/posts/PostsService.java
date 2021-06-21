cbde1b5d164436da39f554594c87ad03106d8d4d
HyeokSeong Kwon <khs50851@naver.com>
2021-06-21 21:25:00 +0900

    등록 API 작성 및 테스트

---
+++ src/main/java/com/hydev/spring_test/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.hydev.spring_test.service.posts;
+
+import com.hydev.spring_test.domain.posts.PostsRepository;
+import com.hydev.spring_test.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
+
---
+++ src/main/java/com/hydev/spring_test/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.hydev.spring_test.web;
+
+import com.hydev.spring_test.service.posts.PostsService;
+import com.hydev.spring_test.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+}
